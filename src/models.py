5f9fdbc80e8374cb96563a599e9652c3a4c93a74
Marian <marianvsf@gmail.com>
2024-10-21 15:11:17 +0000

    correcciones

--- src/models.py
+++ src/models.py
@@ -21,31 +21,63 @@ class User(Base):
     lastname = Column(String(50))
     email = Column(String(150), unique=True, nullable=False)
     
-
 class Character(Base):
     __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
     caption = Column(String(3800))
     image = Column(String(250)) #Url de la imagen del character
-    id_character = Column(Integer, ForeignKey('favorites.id'))
-    Favorite = relationship(Favorite)
+    id_character = Column(Integer, ForeignKey('favoritecharacters.id'))
+    
 
 class Planet(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     caption = Column(String(3800))
     image = Column(String(250)) #Url de la imagen del character
-    id_planet = Column(Integer, ForeignKey('favorites.id'))
-    Favorite = relationship(Favorite)
+    id_planet = Column(Integer, ForeignKey('favoriteplanets.id'))
+    
 
 class Vehicle(Base):
     __tablename__ = 'vehicles'
     id = Column(Integer, primary_key=True)
     caption = Column(String(3800))
     image = Column(String(250)) #Url de la imagen del character
-    id_vehicle = Column(Integer, ForeignKey('favorites.id'))
-    Favorite = relationship(Favorite)
+    id_vehicle = Column(Integer, ForeignKey('favaritevehicles.id'))
+    
 
+class FavoriteCharacter(Base):
+    __tablename__ = 'favoritecharacters'
+    id = Column(Integer, primary_key=True)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    user_id = Column(Integer, ForeignKey('favorites.id'))
+    character = relationship(Character)
+    
+
+    def __repr__(self):
+        return '<FavoriteCharacter %r>' % self.id
+    
+
+class FavoriteVehicle(Base):
+    __tablename__ = 'favoritevehicles'
+    id = Column(Integer, primary_key=True)
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    user_id = Column(Integer, ForeignKey('favorites.id'))
+    vehicle = relationship(Vehicle)
+    
+    
+    def __repr__(self):
+        return '<FavoriteVehicle %r>' % self.id
+    
+class FavoritePlanet(Base):
+    __tablename__ = 'favoriteplanets'
+    id = Column(Integer, primary_key=True)
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    user_id = Column(Integer, ForeignKey('favorites.id'))
+    planet = relationship(Planet)
+    
+    
+    def __repr__(self):
+        return '<FavoritePlanet %r>' % self.id
 
     def to_dict(self):
         return {}
